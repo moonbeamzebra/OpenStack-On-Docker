b2719877a7e3bc2c04c6bb80140b75e5e4e35d8c
jplaberge <moonbeamzebra@magenta.ca>
2015-11-03 21:34:47 -0500

    add creation/deletion scripts

---
+++ kvm_scripts/setup_network.sh
@@ -0,0 +1,30 @@
+source ~/admin-openrc.sh
+
+
+neutron net-create public \
+--shared \
+--provider:physical_network public \
+--provider:network_type flat
+
+
+
+neutron subnet-create public 10.199.5.0/24 \
+--name public \
+--allocation-pool start=10.199.5.80,end=10.199.5.89 \
+--dns-nameserver 8.8.8.8 \
+--gateway 10.199.5.1
+
+neutron net-update public --router:external
+
+source ~/demo-openrc.sh
+
+neutron net-create private
+
+neutron subnet-create private 172.16.1.0/24 --name private --gateway 172.16.1.1
+
+neutron router-create router
+
+neutron router-interface-add router private
+
+neutron router-gateway-set router public
+
