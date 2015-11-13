07ec8a33c03574394f8c806f016259a18cbe6627
jplaberge <moonbeamzebra@magenta.ca>
2015-11-12 22:07:36 -0500

    support of Liberty - Keystone

--- dockerfiles/keystone/setup.sh
+++ dockerfiles/keystone/setup.sh
@@ -1,6 +1,69 @@
 #! /bin/bash
-echo "CREATE DATABASE keystone;GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY '$KEYSTONEDBPASS';GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY '$KEYSTONEDBPASS';FLUSH PRIVILEGES;" | mysql --user=root --password=$MYSQL_ROOT_PASSWORD -h $MYSQLHOST -P 3306
-crudini --set /etc/keystone/keystone.conf database connection mysql://keystone:$KEYSTONEDBPASS@$MYSQLHOST:3306/keystone
-su -s /bin/sh -c "keystone-manage db_sync" keystone
+
+
+cp /etc/keystone/keystone.conf /etc/keystone/keystone.conf.bak
+crudini --set /etc/keystone/keystone.conf DEFAULT admin_token $ADMIN_TOKEN
+crudini --set /etc/keystone/keystone.conf DEFAULT verbose True
+crudini --set /etc/keystone/keystone.conf database connection mysql+pymysql://keystone:$KEYSTONE_DBPASS@$MYSQLHOST/keystone
+crudini --set /etc/keystone/keystone.conf token provider uuid
+crudini --set /etc/keystone/keystone.conf token driver memcache
+crudini --set /etc/keystone/keystone.conf revoke driver sql
+crudini --set /etc/keystone/keystone.conf memcache servers localhost:11211
+diff /etc/keystone/keystone.conf /etc/keystone/keystone.conf.bak
+
+
+cat <<EOF > /etc/apache2/sites-available/wsgi-keystone.conf
+Listen 5000
+Listen 35357
+
+<VirtualHost *:5000>
+    WSGIDaemonProcess keystone-public processes=5 threads=1 user=keystone group=keystone display-name=%{GROUP}
+    WSGIProcessGroup keystone-public
+    WSGIScriptAlias / /usr/bin/keystone-wsgi-public
+    WSGIApplicationGroup %{GLOBAL}
+    WSGIPassAuthorization On
+    <IfVersion >= 2.4>
+      ErrorLogFormat "%{cu}t %M"
+    </IfVersion>
+    ErrorLog /var/log/apache2/keystone.log
+    CustomLog /var/log/apache2/keystone_access.log combined
+
+    <Directory /usr/bin>
+        <IfVersion >= 2.4>
+            Require all granted
+        </IfVersion>
+        <IfVersion < 2.4>
+            Order allow,deny
+            Allow from all
+        </IfVersion>
+    </Directory>
+</VirtualHost>
+
+<VirtualHost *:35357>
+    WSGIDaemonProcess keystone-admin processes=5 threads=1 user=keystone group=keystone display-name=%{GROUP}
+    WSGIProcessGroup keystone-admin
+    WSGIScriptAlias / /usr/bin/keystone-wsgi-admin
+    WSGIApplicationGroup %{GLOBAL}
+    WSGIPassAuthorization On
+    <IfVersion >= 2.4>
+      ErrorLogFormat "%{cu}t %M"
+    </IfVersion>
+    ErrorLog /var/log/apache2/keystone.log
+    CustomLog /var/log/apache2/keystone_access.log combined
+
+    <Directory /usr/bin>
+        <IfVersion >= 2.4>
+            Require all granted
+        </IfVersion>
+        <IfVersion < 2.4>
+            Order allow,deny
+            Allow from all
+        </IfVersion>
+    </Directory>
+</VirtualHost>
+EOF
+
+ln -s /etc/apache2/sites-available/wsgi-keystone.conf /etc/apache2/sites-enabled
+
 
 
