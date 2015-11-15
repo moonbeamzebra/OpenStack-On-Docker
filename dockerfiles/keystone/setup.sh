cc5afbef42cfd40b63ad10fc13cbac4e4018f45b
jplaberge <moonbeamzebra@magenta.ca>
2015-11-15 17:36:38 -0500

    cleanup keystone Dockerfile

--- dockerfiles/keystone/setup.sh
+++ dockerfiles/keystone/setup.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
-echo "manual" > /etc/init/keystone.override
+## Set up keystone
+echo "CREATE DATABASE keystone;
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY '$KEYSTONE_DBPASS';
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY '$KEYSTONE_DBPASS';
+FLUSH PRIVILEGES;" | mysql --user=root --password=$MYSQL_ROOT_PASSWORD -h $MYSQLHOST -P 3306
 
+echo "manual" > /etc/init/keystone.override
 
 cp /etc/keystone/keystone.conf /etc/keystone/keystone.conf.bak
 crudini --set /etc/keystone/keystone.conf DEFAULT admin_token $ADMIN_TOKEN
@@ -13,6 +18,9 @@ crudini --set /etc/keystone/keystone.conf revoke driver sql
 crudini --set /etc/keystone/keystone.conf memcache servers localhost:11211
 diff /etc/keystone/keystone.conf /etc/keystone/keystone.conf.bak
 
+sleep 5
+
+su -s /bin/sh -c "keystone-manage db_sync" keystone
 
 cat <<EOF > /etc/apache2/sites-available/wsgi-keystone.conf
 Listen 5000
@@ -67,6 +75,44 @@ EOF
 
 ln -s /etc/apache2/sites-available/wsgi-keystone.conf /etc/apache2/sites-enabled
 
+service apache2 restart
+rm -f /var/lib/keystone/keystone.db
+
+sleep 5
+
+export OS_TOKEN=$ADMIN_TOKEN
+export OS_URL=http://$KEYSTONE_HOST:35357/v3
+export OS_IDENTITY_API_VERSION=3
+
+openstack service create \
+  --name keystone --description "OpenStack Identity" identity
+  
+openstack endpoint create --region $REGION1 \
+  identity public http://$KEYSTONE_HOST:5000/v2.0
+openstack endpoint create --region $REGION1 \
+  identity internal http://$KEYSTONE_HOST:5000/v2.0   
+openstack endpoint create --region $REGION1 \
+  identity admin http://$KEYSTONE_HOST:35357/v2.0   
+openstack project create --domain default \
+  --description "Admin Project" admin
+openstack user create  --domain default --password $ADMIN_PASS admin
+openstack role create admin
+openstack role add --project admin --user admin admin
+openstack project create --domain default \
+  --description "Service Project" service
+openstack project create --domain default \
+  --description "Demo Project" demo
+openstack user create  --domain default --password $DEMO_PASS demo
+openstack role create user
+openstack role add --project demo --user demo user
+
+
+cp /etc/keystone/keystone-paste.ini /etc/keystone/keystone-paste.ini.bak
+crudini --del /etc/keystone/keystone-paste.ini pipeline:public_api admin_token_auth
+crudini --del /etc/keystone/keystone-paste.ini pipeline:admin_api admin_token_auth
+crudini --del /etc/keystone/keystone-paste.ini pipeline:api_v3 admin_token_auth
+diff /etc/keystone/keystone-paste.ini /etc/keystone/keystone-paste.ini.bak
+
 cat <<EOF > /admin-openrc.sh
 export OS_PROJECT_DOMAIN_ID=default
 export OS_USER_DOMAIN_ID=default
@@ -90,5 +136,6 @@ export OS_AUTH_URL=http://$KEYSTONE_HOST:5000/v3
 export OS_IDENTITY_API_VERSION=3
 export OS_IMAGE_API_VERSION=2
 EOF
+
 
 
