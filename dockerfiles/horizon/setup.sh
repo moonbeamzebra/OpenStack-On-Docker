3856ad610a5a8d981f632803beee0f5816d08773
jplaberge <moonbeamzebra@magenta.ca>
2015-11-15 20:56:28 -0500

    set OPENSTACK_HOST

--- dockerfiles/horizon/setup.sh
+++ dockerfiles/horizon/setup.sh
@@ -32,7 +32,7 @@ EOF
 
 
 #sed -i.bak "s/ALLOWED_HOSTS = \['horizon.example.com', 'localhost'\]/ALLOWED_HOSTS = ['*']/" /etc/openstack-dashboard/local_settings.py
-#sed -i 's/OPENSTACK_HOST = "127.0.0.1"/OPENSTACK_HOST = "'"$KEYSTONE_HOST"'"/' /etc/openstack-dashboard/local_settings.py
+sed -i 's/OPENSTACK_HOST = "127.0.0.1"/OPENSTACK_HOST = "'"$KEYSTONE_HOST"'"/' /etc/openstack-dashboard/local_settings.py
 
 touch /setup.done
 
